6de3316663e6fa50b1c8af7ea880ea5282259aea
joao.coelh0 <joaopedroduarte.coelho@gmail.com>
2021-05-18 11:02:37 +0100

    dijkstra

---
+++ dijkstra.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+ * (c) 2021 João Nabais & João Coelho
+ *
+ * NAME
+ *   dijsktra.c
+ *
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "fileData.h"
+#include "graphs.h"
+#include "matrix.h"
+#include "problems.h"
+#include "binaryTree.h"
+
---
+++ dijkstra.h
@@ -0,0 +1,9 @@
+#ifndef DIJKSTRA_H
+#define DIJKSTRA_H
+
+#include "graphs.h"
+#include "binaryTree.h"
+
+
+
+#endif
